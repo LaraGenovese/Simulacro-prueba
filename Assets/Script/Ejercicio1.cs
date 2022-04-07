6434f304182f0757f87eb1c9097f55d9d08ace10
LaraGenovese <93852155+LaraGenovese@users.noreply.github.com>
2022-04-07 08:41:26 -0300

    Finnished exercises 1 and 2

---
+++ Assets/Script/Ejercicio1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //Crea un programa que pida dos números al usuario y responda si son iguales o no lo son.
+
+    public int numero1;
+    public int numero2;
+    
+    void Start()
+    {
+        if(numero1 == numero2)
+        {
+            Debug.Log("Los números son iguales");
+        }
+        else
+        {
+            Debug.Log("Los números no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Ejercicio2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //Solicitar el ingreso del precio de 3 productos y un monto de dinero disponible. Comunicar si la suma de los mismos supera o no el monto de dinero disponible. Luego indicar cuánto dinero sobra o falta.
+
+    public float producto1, producto2, producto3, dinero_disponible;
+
+    void Start()
+    {
+        dinero_disponible = dinero_disponible - (producto1 + producto2 + producto3);
+
+        if (dinero_disponible >= 0)
+        {
+            Debug.Log("La suma de los 3 productos NO supera el monto de dinero disponible");
+            Debug.Log("Sobran: $" + dinero_disponible);
+        }
+        else
+        {
+            Debug.Log("La suma de los 3 productos SI supera el monto de dinero disponible");
+            Debug.Log("Faltan: $" + dinero_disponible * -1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
